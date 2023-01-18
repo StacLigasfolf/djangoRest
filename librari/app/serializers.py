42f574989bddf4f58b161fe3d566a7ca98d49c3d
Mark Liman <65871508+StacLigasfolf@users.noreply.github.com>
2023-01-18 19:10:32 +0300

    done

--- librari/app/serializers.py
+++ librari/app/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
-from .models import Author
+from rest_framework import serializers
+
+from .models import Author, Book, Biography, Article
 
 
 class AuthorModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Author
         fields = '__all__'
+        
+class BiographySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Biography
+        fields = '__all__'
+
+class ArticleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Article
